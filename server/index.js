c2d2db2b4eced6c1df81d6afc9c4e8e88670370e
AndjDavis <andi.idavis@gmail.com>
2024-10-29 15:03:51 -0600

    hooked up apollo server, express middleware, and connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,14 +1,41 @@
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import cors from "cors";
+
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
+import { connectDB } from "./config/db.conf.js";
 
 import mergedResolvers from "./resolvers/index.js";
 import mergedTypeDefs from "./typeDefs/index.js";
 
+const app = express();
+const httpServer = http.createServer(app);
+
 const server = new ApolloServer({
 	typeDefs: mergedTypeDefs,
 	resolvers: mergedResolvers,
+	plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server);
+await server.start();
 
-console.log(`🚀 Server ready at ${url}`);
+app.use(
+	"/",
+	cors(),
+	express.json(),
+	// expressMiddleware accepts the same arguments:
+	// an Apollo Server instance and optional configuration options
+	expressMiddleware(server, {
+		context: async ({ req }) => ({ req }),
+	})
+);
+
+const PORT = 4000;
+await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+await connectDB();
+
+console.log(`🚀 Server ready on port ${PORT}`);
