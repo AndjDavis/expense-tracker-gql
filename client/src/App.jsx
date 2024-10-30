deb2831a48e83318af459f50583101e2c4f6b6d4
AndjDavis <andi.idavis@gmail.com>
2024-10-30 14:54:03 -0600

    added router and routes to app and main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,82 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { Toaster } from "react-hot-toast";
+
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import TransactionPage from "./pages/TransactionPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import Header from "./components/ui/Header";
+
+// import { GET_AUTHENTICATED_USER } from "./graphql/queries/user.query";
+
 function App() {
+	const authUser = true;
 	return (
 		<>
-			<div>
-				<h1 className="text-red-500 text-5xl">Hello World</h1>
-			</div>
+			{authUser && <Header />}
+			<Routes>
+				<Route
+					path="/"
+					element={<HomePage />}
+				/>
+				<Route
+					path="/login"
+					element={<LoginPage />}
+				/>
+				<Route
+					path="/signup"
+					element={<SignUpPage />}
+				/>
+				<Route
+					path="/transaction/:id"
+					element={<TransactionPage />}
+				/>
+				<Route
+					path="*"
+					element={<NotFoundPage />}
+				/>
+			</Routes>
 		</>
 	);
 }
+
+// function App() {
+// 	const { loading, data } = useQuery(GET_AUTHENTICATED_USER);
+
+// 	if (loading) return null;
+
+// 	return (
+// 		<>
+// 			{data?.authUser && <Header />}
+// 			<Routes>
+// 				<Route
+// 					path="/"
+// 					element={data.authUser ? <HomePage /> : <Navigate to="/login" />}
+// 				/>
+// 				<Route
+// 					path="/login"
+// 					element={!data.authUser ? <LoginPage /> : <Navigate to="/" />}
+// 				/>
+// 				<Route
+// 					path="/signup"
+// 					element={!data.authUser ? <SignUpPage /> : <Navigate to="/" />}
+// 				/>
+// 				<Route
+// 					path="/transaction/:id"
+// 					element={
+// 						data.authUser ? <TransactionPage /> : <Navigate to="/login" />
+// 					}
+// 				/>
+// 				<Route
+// 					path="*"
+// 					element={<NotFoundPage />}
+// 				/>
+// 			</Routes>
+// 			<Toaster />
+// 		</>
+// 	);
+// }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App.jsx";
+import "./index.css";
+import GridBackground from "./components/ui/GridBackground.jsx";
+
+const SERVER_BASE_URL = import.meta.VITE_SERVER_BASE_URL;
+const GRAPHQL_URI = "/graphql";
+
+// const client = new ApolloClient({
+// 	// TODO => Update the uri on production
+// 	uri:
+// 		import.meta.env.VITE_NODE_ENV === "development"
+// 			? `${SERVER_BASE_URL}${GRAPHQL_URI}`
+// 			: GRAPHQL_URI, // the URL of our GraphQL server.
+// 	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
+// 	credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
+// });
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+// 	<React.StrictMode>
+// 		<BrowserRouter>
+// 			<GridBackground>
+// 				<ApolloProvider client={client}>
+// 					<App />
+// 				</ApolloProvider>
+// 			</GridBackground>
+// 		</BrowserRouter>
+// 	</React.StrictMode>
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<GridBackground>
+				<App />
+			</GridBackground>
+		</BrowserRouter>
+	</React.StrictMode>
+);
