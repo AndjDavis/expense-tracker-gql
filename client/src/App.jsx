9170856cbf9d06d50757118ccd79757b747f3617
AndjDavis <andi.idavis@gmail.com>
2024-11-04 14:44:22 -0700

    apollo client added to main.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import { useQuery } from "@apollo/client";
+import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import HomePage from "./pages/HomePage";
@@ -8,75 +7,48 @@ import SignUpPage from "./pages/SignUpPage";
 import TransactionPage from "./pages/TransactionPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import Header from "./components/ui/Header";
+import HomeSkeleton from "./components/skeletons/HomeSkeleton";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import PersistLogin from "./routes/PersistLogin";
 
-// import { GET_AUTHENTICATED_USER } from "./graphql/queries/user.query";
+import { useAuth } from "./hooks/useAuth";
 
 function App() {
-	const authUser = true;
+	const { loading, data } = useAuth();
+	if (loading) return <HomeSkeleton />;
+	const authUser = data?.authUser;
 	return (
 		<>
 			{authUser && <Header />}
 			<Routes>
-				<Route
-					path="/"
-					element={<HomePage />}
-				/>
-				<Route
-					path="/login"
-					element={<LoginPage />}
-				/>
-				<Route
-					path="/signup"
-					element={<SignUpPage />}
-				/>
-				<Route
-					path="/transaction/:id"
-					element={<TransactionPage />}
-				/>
+				<Route element={<PersistLogin authUser={authUser} />}>
+					<Route
+						path="/login"
+						element={<LoginPage />}
+					/>
+					<Route
+						path="/signup"
+						element={<SignUpPage />}
+					/>
+				</Route>
+				<Route element={<ProtectedRoute authUser={authUser} />}>
+					<Route
+						path="/"
+						element={<HomePage />}
+					/>
+					<Route
+						path="/transaction/:id"
+						element={<TransactionPage />}
+					/>
+				</Route>
 				<Route
 					path="*"
 					element={<NotFoundPage />}
 				/>
 			</Routes>
+			<Toaster />
 		</>
 	);
 }
-
-// function App() {
-// 	const { loading, data } = useQuery(GET_AUTHENTICATED_USER);
-
-// 	if (loading) return null;
-
-// 	return (
-// 		<>
-// 			{data?.authUser && <Header />}
-// 			<Routes>
-// 				<Route
-// 					path="/"
-// 					element={data.authUser ? <HomePage /> : <Navigate to="/login" />}
-// 				/>
-// 				<Route
-// 					path="/login"
-// 					element={!data.authUser ? <LoginPage /> : <Navigate to="/" />}
-// 				/>
-// 				<Route
-// 					path="/signup"
-// 					element={!data.authUser ? <SignUpPage /> : <Navigate to="/" />}
-// 				/>
-// 				<Route
-// 					path="/transaction/:id"
-// 					element={
-// 						data.authUser ? <TransactionPage /> : <Navigate to="/login" />
-// 					}
-// 				/>
-// 				<Route
-// 					path="*"
-// 					element={<NotFoundPage />}
-// 				/>
-// 			</Routes>
-// 			<Toaster />
-// 		</>
-// 	);
-// }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,36 +7,22 @@ import App from "./App.jsx";
 import "./index.css";
 import GridBackground from "./components/ui/GridBackground.jsx";
 
-const SERVER_BASE_URL = import.meta.VITE_SERVER_BASE_URL;
-const GRAPHQL_URI = "/graphql";
+const SERVER_BASE_URL = import.meta.env.VITE_SERVER_BASE_URL;
+const BACKEND_URI = SERVER_BASE_URL + "/graphql";
 
-// const client = new ApolloClient({
-// 	// TODO => Update the uri on production
-// 	uri:
-// 		import.meta.env.VITE_NODE_ENV === "development"
-// 			? `${SERVER_BASE_URL}${GRAPHQL_URI}`
-// 			: GRAPHQL_URI, // the URL of our GraphQL server.
-// 	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
-// 	credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
-// });
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-// 	<React.StrictMode>
-// 		<BrowserRouter>
-// 			<GridBackground>
-// 				<ApolloProvider client={client}>
-// 					<App />
-// 				</ApolloProvider>
-// 			</GridBackground>
-// 		</BrowserRouter>
-// 	</React.StrictMode>
-// );
+const client = new ApolloClient({
+	uri: BACKEND_URI, // the URL of our GraphQL server.
+	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
+	credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<GridBackground>
-				<App />
+				<ApolloProvider client={client}>
+					<App />
+				</ApolloProvider>
 			</GridBackground>
 		</BrowserRouter>
 	</React.StrictMode>
