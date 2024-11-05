b927a1e5e7da6d191c955eb9c05da5f12ba3542e
AndjDavis <andi.idavis@gmail.com>
2024-11-05 14:20:16 -0700

    updated card components to use transaction data, mutations, and queries

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import toast from "react-hot-toast";
+
 import { FaLocationDot } from "react-icons/fa6";
 import { BsCardText } from "react-icons/bs";
 import { MdOutlinePayments } from "react-icons/md";
 import { FaSackDollar } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { HiPencilAlt } from "react-icons/hi";
+
+import Spinner from "./Spinner";
+import { DELETE_TRANSACTION } from "../graphql/mutations/transaction.mutation";
+import { formatDate } from "../utils/format";
 
 const categoryColorMap = {
 	saving: "from-green-700 to-green-400",
@@ -13,44 +20,89 @@ const categoryColorMap = {
 	// Add more categories and corresponding color classes as needed
 };
 
-const Card = ({ cardType }) => {
-	const cardClass = categoryColorMap[cardType];
+const Card = ({ transaction, authUser }) => {
+	const [deleteTransaction, { loading }] = useMutation(DELETE_TRANSACTION, {
+		refetchQueries: ["GetTransactions"],
+	});
+
+	const {
+		amount,
+		category,
+		date,
+		description,
+		location,
+		paymentType,
+		_id: transactionId,
+	} = transaction;
+
+	const cardClass = categoryColorMap[category];
+	const formattedDate = formatDate(date);
+	const profilePicture = authUser?.profilePicture || "";
+
+	const handleDelete = async () => {
+		try {
+			await deleteTransaction({
+				variables: { transactionId: transactionId },
+			});
+			toast.success("Transaction deleted successfully");
+		} catch (error) {
+			console.error("Error deleting transaction:", error);
+			toast.error(error.message);
+		}
+	};
+
+	let cardIcons = (
+		<div className="flex items-center gap-2">
+			<Spinner />
+		</div>
+	);
+
+	if (!loading && transactionId) {
+		cardIcons = (
+			<div className="flex items-center gap-2">
+				<FaTrash
+					className={"cursor-pointer"}
+					onClick={handleDelete}
+				/>
+				<Link to={`/transaction/${transactionId}`}>
+					<HiPencilAlt
+						className="cursor-pointer"
+						size={20}
+					/>
+				</Link>
+			</div>
+		);
+	}
 
 	return (
 		<div className={`rounded-md p-4 bg-gradient-to-br ${cardClass}`}>
 			<div className="flex flex-col gap-3">
 				<div className="flex flex-row items-center justify-between">
-					<h2 className="text-lg font-bold text-white">Saving</h2>
-					<div className="flex items-center gap-2">
-						<FaTrash className={"cursor-pointer"} />
-						<Link to={`/transaction/123`}>
-							<HiPencilAlt
-								className="cursor-pointer"
-								size={20}
-							/>
-						</Link>
-					</div>
+					<h2 className="text-lg font-bold text-white capitalize">
+						{category}
+					</h2>
+					{cardIcons}
 				</div>
-				<p className="text-white flex items-center gap-1">
+				<p className="text-white flex items-center gap-1 capitalize">
 					<BsCardText />
-					Description: Salary
+					Description: {description}
 				</p>
-				<p className="text-white flex items-center gap-1">
+				<p className="text-white flex items-center gap-1 capitalize">
 					<MdOutlinePayments />
-					Payment Type: Cash
+					Payment Type: {paymentType}
 				</p>
 				<p className="text-white flex items-center gap-1">
 					<FaSackDollar />
-					Amount: $150
+					Amount: ${amount}
 				</p>
-				<p className="text-white flex items-center gap-1">
+				<p className="text-white flex items-center gap-1 capitalize">
 					<FaLocationDot />
-					Location: New York
+					Location: {location || "N/A"}
 				</p>
 				<div className="flex justify-between items-center">
-					<p className="text-xs text-black font-bold">21 Sep, 2001</p>
+					<p className="text-xs text-black font-bold">{formattedDate}</p>
 					<img
-						src={"https://tecdn.b-cdn.net/img/new/avatars/2.webp"}
+						src={profilePicture}
 						className="h-8 w-8 border rounded-full"
 						alt=""
 					/>
